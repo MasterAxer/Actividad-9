8e74d07f7cc3718949eeeb7f895755be64055e7d
Emmanuel Gonzalez <33494441+jgonzalez13@users.noreply.github.com>
2020-10-28 09:39:19 -0600

    Update func.js
    asi te quedara mejor jajaja, Don't repeat yourself

--- js/func.js
+++ js/func.js
@@ -2,45 +2,35 @@ document.getElementById('sumar').onclick=function(){operaciones(1)};
 document.getElementById('restar').onclick=function(){operaciones(2)};
 document.getElementById('multiplicar').onclick=function(){operaciones(3)};
 document.getElementById('dividir').onclick=function(){operaciones(4)};
+
 function operaciones(option)
 {
+  var resultado = document.getElementById('res');
+  var resultado2 = document.getElementById('res2');
+  var num1=prompt("Dame el 1er número:");
+  var num2=prompt("Dame el 2do número:");
+
 	switch(option)
 	{
 		case 1:
-			var resultado = document.getElementById('res');
-			var resultado2 = document.getElementById('res2');
-			var num1=prompt("Dame el 1er número:");
-			var num2=prompt("Dame el 2do número:");
 			var suma = parseInt(num1) + parseInt(num2);
 			resultado.innerHTML="La suma de "+ num1 + " + " + num2 + " es igual a: " + suma;
 			resultado2.innerHTML="";
 			break;
 		case 2:
-			var resultado = document.getElementById('res');
-			var resultado2 = document.getElementById('res2');
-			var num1=prompt("Dame el 1er número:");
-			var num2=prompt("Dame el 2do número:");
 			var resta = parseInt(num1) - parseInt(num2);
 			resultado.innerHTML="La resta de "+ num1 + " - " + num2 + " es igual a: " + resta;
 			resultado2.innerHTML="";
 			break;
 		case 3:
-			var resultado = document.getElementById('res');
-			var resultado2 = document.getElementById('res2');
-			var num1=prompt("Dame el 1er número:");
-			var num2=prompt("Dame el 2do número:");
 			var mult = parseInt(num1) * parseInt(num2);
 			resultado.innerHTML="La multiplicación de "+ num1 + " x " + num2 + " es igual a: " + mult;
 			resultado2.innerHTML="";
 			break;
 		case 4:
-			var resultado = document.getElementById('res');
-			var resultado2 = document.getElementById('res2');
-			var num1=prompt("Dame el 1er número:");
-			var num2=prompt("Dame el 2do número:");
 			var divtion = parseInt(num1) / parseInt(num2);
 			resultado.innerHTML="La división de "+ num1 + " ÷ " + num2 + " es igual a: " + divtion;
 			resultado2.innerHTML="El sobrante es: " + (parseInt(num1)%parseInt(num2));
-			break;	
+			break;
 	}
 }
